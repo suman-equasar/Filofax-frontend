80e74fd072f87ee97602ff50857929475ecc7e94
suman <sumanequasar@gmail.com>
2025-05-22 11:24:42 +0530

    first commit

--- src/user/Booking.jsx
+++ src/user/Booking.jsx
@@ -6,7 +6,7 @@ const Booking = () => {
   return (
     <div className="container mx-auto px-8 py-8 flex flex-col items-center justify-center">
       <img src={logo} alt="logo" className="w-12 h-12 mb-2" />
-      <span className="text-md font-bold text-center text-gray-400 mb-2">
+      <span className="text-md font-bold text-center text-gray-400 mb-2 ">
         ACMC Inc.
       </span>
       <h1 className="text-2xl font-bold text-center mb-4"> Product Demo </h1>
